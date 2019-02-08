a64d3c73eda8d26fa22a89c48b67ade92f13d68b
mohamedhanaz <42069234+mohamedhanaz@users.noreply.github.com>
2019-02-08 11:46:54 +0530

    Update AlphabetOrNot.py

--- AlphabetOrNot.py
+++ AlphabetOrNot.py
@@ -1,4 +1,4 @@
-l=str(input("Input a letter of the alphabet:"))
+l=str(input())
 if l in ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'):
   print("Alphabet")
 else:
